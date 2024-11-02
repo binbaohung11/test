df7dfa56163b5c568e7506d9d0061fe4b3f4544d
Juno270302 <hungnxb2703@gmail.com>
2024-11-02 14:39:06 +0700

    fix cai gi do

--- app/[locale]/our/introduction/page.tsx
+++ app/[locale]/our/introduction/page.tsx
@@ -4,41 +4,8 @@ import Materials from "@/components/Our/Introduction/Materials";
 import Product from "@/components/Our/Introduction/Product";
 import Structure from "@/components/Our/Introduction/Structure";
 import PathLink from "@/components/Path/PathLink";
-import { Metadata } from "next";
-import { useTranslations } from "next-intl";
 import Head from "next/head";
 import React from "react";
-
-// export const metadata: Metadata = {
-//   title: "Introduction - Công ty TNHH Xuất nhập khẩu Cao Nguyên Bình Phước",
-//   description:
-//     "Highland Bp., Ltd was established on February 27, 2019, Business registration certificate number: 3801195652 Binh Phuoc Department of Planning and Investment.",
-//   openGraph: {
-//     title: "Introduction - Công ty TNHH Xuất nhập khẩu Cao Nguyên Bình Phước",
-//     description:
-//       "Highland Bp., Ltd was established on February 27, 2019, Business registration certificate number: 3801195652 Binh Phuoc Department of Planning and Investment.",
-//     url: "https://highlandbp.com.vn/introduction",
-//     type: "article",
-//     locale: "en_US",
-//     images: [
-//       {
-//         url: "https://highlandbp.com.vn/wp-content/uploads/2020/07/than-gao-dua-vuong.jpg",
-//         width: 810,
-//         height: 463,
-//         alt: "Introduction",
-//         type: "image/jpeg",
-//       },
-//     ],
-//   },
-//   twitter: {
-//     card: "summary_large_image",
-//     title: "Introduction - Công ty TNHH Xuất nhập khẩu Cao Nguyên Bình Phước",
-//     description:
-//       "Highland Bp., Ltd was established on February 27, 2019, Business registration certificate number: 3801195652 Binh Phuoc Department of Planning and Investment.",
-//     images:
-//       "https://highlandbp.com.vn/wp-content/uploads/2020/07/than-gao-dua-vuong.jpg",
-//   },
-// };
 
 const Introduction = () => {
   return (
